b4dd7b4bf2962d1ace7898d5048f49fbf4cc4961
Arbin89 <arbintejada8@gmail.com>
2024-06-18 20:22:58 -0400

    Paredes con colisiones

--- tablero.py
+++ tablero.py
@@ -8,6 +8,7 @@ class TABLERO:
         self.meta = None
         self.obstaculos = []
         self.vehiculos = []
+        self.paredes = []
         self.filas = 0
         self.columnas = 0
         self.nombreTxt = nombre_txt
@@ -20,13 +21,6 @@ class TABLERO:
                     self.columnas = len(linea)
                 fila_tablero = []
 
-                #! Por si no entiende:
-                """
-                    Aqui aparte del cometido de la funcion, se le agrega : 'orientacion': None
-                    Eso es porque o para, xd
-                    en el txt no se coloca explicitamente la orientacion y pues mas abajo hay una funcion que ferifica si
-                    el carro esta en vertical u horizontal... Abajo ta como funciona
-                """
                 for columna, caracter in enumerate(linea):
                     posicion = {'fila': fila + 1, 'columna': columna + 1}
                     fila_tablero.append(caracter)
@@ -37,6 +31,8 @@ class TABLERO:
                         self.meta = posicion
                     elif caracter == 'B':
                         self.obstaculos.append({'tipo': 'obstaculo', 'posicion': posicion})
+                    elif caracter == '#':
+                        self.paredes.append({'tipo': 'pared', 'posicion': posicion})
                     elif caracter.isalpha() and (caracter.islower() or (caracter.isupper() and caracter not in {'A', 'B'})):
                         self.vehiculos.append({'tipo': 'vehiculo', 'caracter': caracter, 'posicion': posicion, 'orientacion': None})
                 self.tablero.append(fila_tablero)
@@ -44,34 +40,38 @@ class TABLERO:
         self.actualizarOrientacionVehiculos()
 
     def actualizarOrientacionVehiculos(self):
-        #! Finciona tan que así:
-        """
-           El verifica su arededor, si el carro tiene arriba... un caracter igual al que tiene,
-           pues se determina si esta horizontal o vertical
-
-           Se entendió?
-        """
         for vehiculo in self.vehiculos:
             fila, columna = vehiculo['posicion']['fila'], vehiculo['posicion']['columna']
             caracter = vehiculo['caracter']
-            # Revisar atras y alante
             if columna < self.columnas and self.tablero[fila - 1][columna] == caracter:
                 vehiculo['orientacion'] = 'horizontal'
-            # Revisar arriba y abajo
             elif fila < self.filas and self.tablero[fila][columna - 1] == caracter:
                 vehiculo['orientacion'] = 'vertical'
-            # Revisar atras
             elif columna > 1 and self.tablero[fila - 1][columna - 2] == caracter:
                 vehiculo['orientacion'] = 'horizontal'
-            # Revisar arriba
             elif fila > 1 and self.tablero[fila - 2][columna - 1] == caracter:
                 vehiculo['orientacion'] = 'vertical'
-        print( vehiculo['orientacion'])
 
     def getInfo(self):
-        return self.vehiculo_jugador, self.meta, self.obstaculos, self.vehiculos, self.filas, self.columnas
+        return self.vehiculo_jugador, self.meta, self.obstaculos, self.paredes, self.vehiculos, self.filas, self.columnas
 
     def actualizarVehiculo(self, vehiculo, nueva_posicion):
+        # Verifica si la nueva posición es una pared u otro obstáculo
+        for pared in self.paredes:
+            if pared['posicion'] == nueva_posicion:
+                print("Movimiento inválido: hay una pared en la nueva posición.")
+                return False  # Movimiento inválido
+
+        for obstaculo in self.obstaculos:
+            if obstaculo['posicion'] == nueva_posicion:
+                print("Movimiento inválido: hay un obstáculo en la nueva posición.")
+                return False  # Movimiento inválido
+
+        for v in self.vehiculos:
+            if v['posicion'] == nueva_posicion:
+                print("Movimiento inválido: hay otro vehículo en la nueva posición.")
+                return False  # Movimiento inválido
+
         posicion_anterior = None
         if vehiculo == 'A':
             posicion_anterior = self.vehiculo_jugador
@@ -86,10 +86,9 @@ class TABLERO:
         if posicion_anterior:
             self.tablero[posicion_anterior['fila'] - 1][posicion_anterior['columna'] - 1] = '.'
         self.tablero[nueva_posicion['fila'] - 1][nueva_posicion['columna'] - 1] = vehiculo
+        return True  # Movimiento válido
 
     def dibujarTablero(self):
-        #! Peligro!!
-        #!No cambien nada aquí
         self.cls()
         print("   ", end=" ")
         for col in range(1, self.columnas + 1):
@@ -107,3 +106,16 @@ class TABLERO:
     def cls(self):
         if os.name == 'nt':
             os.system('cls')
+
+# Ejemplo de uso
+tablero = TABLERO("controlador.txt")
+tablero.dimensionarTablero()
+tablero.dibujarTablero()
+
+# Intentar mover el vehículo 'A' a una nueva posición (ejemplo)
+nueva_posicion = {'fila': 2, 'columna': 3}
+if tablero.actualizarVehiculo('A', nueva_posicion):
+    print("Movimiento realizado con éxito.")
+else:
+    print("Movimiento no permitido.")
+tablero.dibujarTablero()
